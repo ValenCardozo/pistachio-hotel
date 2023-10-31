32b76f49f2a0cde31c63c4e3e8fa9fcd9628389e
matiasg99 <matiias.gomeez999@gmail.com>
2023-10-31 20:45:27 -0300

    refactor class admin, and fix visual

--- admin.py
+++ admin.py
@@ -4,6 +4,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from database import getAllRooms, deleteRoom, insertRoom, updateRoomById
 from UpdateRoom import *
+from CustomWidgets import StyledWidget, StyledLabel
 
 class Admin(QMainWindow):
     def __init__(self):
@@ -14,35 +15,28 @@ class Admin(QMainWindow):
         self.setStyleSheet("background-color: #ffffff; color: #000000;")
 
     def initUI(self):
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
+        self.create_header_widget()
+        self.create_input_widgets()
+        self.create_layouts()
+        self.connect_signals()
+        self.populate_rooms()
 
-        main_layout = QVBoxLayout(central_widget)
-        left_layout = QVBoxLayout()
-        right_layout = QVBoxLayout()
+    def create_header_widget(self):
+        self.headerWidget = StyledWidget()
+        self.headerWidget.setStyleSheet("background-color: #8db600; color: white; font-weight: bold;")
+        self.headerWidget.setFixedHeight(90)
 
-        header_frame = QFrame()
-        header_frame.setFrameShape(QFrame.StyledPanel)
-        header_frame.setFrameShadow(QFrame.Raised)
-        header_frame.setMaximumHeight(80)
-        header_frame.setStyleSheet("background-color: #8db600; color: white; font-weight: bold; border: 1px solid black;")
-        header_layout = QHBoxLayout(header_frame)
+        header_layout = QHBoxLayout(self.headerWidget)
+        header_layout.setContentsMargins(0, 0, 0, 0)
+        header_layout.setSpacing(1)
 
-        title_label = QLabel("Hotel Pistacho Administración", self)
-        title_label.setAlignment(Qt.AlignLeft)
-        title_font = QFont()
-        title_font.setPointSize(15)
-        title_font.setBold(True)
-        title_label.setFont(title_font)
-        title_label.setFixedHeight(20)
+        title_label = StyledLabel("Hotel Pistacho Administración")
+        title_label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
         header_layout.addWidget(title_label)
-        header_layout.addStretch(1)
 
-        main_layout.addWidget(header_frame)
-
+    def create_input_widgets(self):
         self.input_group = QGroupBox("Detalles de la habitación")
         self.input_group.setStyleSheet("border: 1px solid #d3d3d3; margin-top: 0.5em;")
-
         self.input_group.setFixedHeight(400)
         input_layout = QVBoxLayout(self.input_group)
 
@@ -64,9 +58,9 @@ class Admin(QMainWindow):
         self.price_input = QLineEdit(self)
         self.price_input.setFixedHeight(25)
 
-        create_button = QPushButton("Crear", self)
-        create_button.setFixedWidth(200)
-        create_button.setStyleSheet("background-color: #c3d9af;")
+        self.create_button = QPushButton("Crear", self)
+        self.create_button.setFixedWidth(200)
+        self.create_button.setStyleSheet("background-color: #c3d9af;")
 
         input_layout.addWidget(cant_personas_label)
         input_layout.addWidget(self.cant_personas_input)
@@ -74,7 +68,20 @@ class Admin(QMainWindow):
         input_layout.addWidget(self.description_input)
         input_layout.addWidget(price_label)
         input_layout.addWidget(self.price_input)
-        input_layout.addWidget(create_button)
+        input_layout.addWidget(self.create_button)
+
+    def create_layouts(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+
+        main_layout = QVBoxLayout(central_widget)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.setSpacing(0)
+
+        left_layout = QVBoxLayout()
+        right_layout = QVBoxLayout()
+
+        main_layout.addWidget(self.headerWidget)
 
         left_layout.addWidget(self.input_group)
 
@@ -86,16 +93,15 @@ class Admin(QMainWindow):
         h_layout.addStretch(1)
         h_layout.addLayout(right_layout)
 
+        main_layout.addStretch(1)
         main_layout.addLayout(h_layout)
 
-        self.populate_rooms()
-
-        create_button.clicked.connect(self.add_room)
+    def connect_signals(self):
+        self.create_button.clicked.connect(self.add_room)
 
     def populate_rooms(self):
         for i in reversed(range(self.rooms_layout.count())):
             item = self.rooms_layout.itemAt(i)
-
             if isinstance(item, QWidgetItem):
                 widget = item.widget()
                 if widget is not None:
@@ -111,47 +117,44 @@ class Admin(QMainWindow):
                     item.takeAt(0)
 
         rooms = getAllRooms()
-        if rooms != []:
+        if rooms:
             for room in rooms:
-                id, description, cantPersons, price = room[0], room[1], room[2], room[3]
+                self.populate_room_item(room)
 
-                room_frame = QFrame(self)
-                room_frame.setFrameShape(QFrame.StyledPanel)
-                room_frame.setFrameShadow(QFrame.Raised)
-                room_frame_layout = QVBoxLayout(room_frame)
-                room_frame.setMaximumHeight(50)
-                room_frame.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-                room_frame.setStyleSheet("border: 1px solid black;")
+    def populate_room_item(self, room):
+        id, description, cantPersons, price = room
+        room_frame = self.create_room_frame(id, description, cantPersons, price)
+        self.rooms_layout.addWidget(room_frame)
 
-                item_layout = QHBoxLayout()
-                item_label = QLabel(f" {description} - {cantPersons} personas - ${price}")
-                self.update_button = QPushButton("Actualizar")
-                self.update_button.setFixedWidth(75)
-                self.update_button.setStyleSheet("background-color: #99dee6; color: #ffffff;")
-                self.update_button.clicked.connect(lambda
-                    room_id=id,
-                    desc=description,
-                    persons=cantPersons,
-                    price=price: self.show_update_dialog(room_id, desc, persons, price)
-                )
+    def create_room_frame(self, room_id, description, cantPersons, price):
+        room_frame = QFrame(self)
+        room_frame.setFrameShape(QFrame.StyledPanel)
+        room_frame.setFrameShadow(QFrame.Raised)
+        room_frame_layout = QVBoxLayout(room_frame)
+        room_frame.setMaximumHeight(50)
+        room_frame.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        room_frame.setStyleSheet("border: 1px solid black;")
 
-                delete_button = QPushButton("Borrar")
-                delete_button.setFixedWidth(75)
-                delete_button.setStyleSheet("background-color: #ff0125; color: #ffffff;")
+        item_layout = QHBoxLayout()
+        item_label = QLabel(f" {description} - {cantPersons} personas - ${price}")
 
-                delete_button.clicked.connect(lambda
-                    desc=description,
-                    persons=cantPersons,
-                    price_val=price: self.delete_room(desc, persons, price_val)
-                )
+        self.update_button = QPushButton("Actualizar")
+        self.update_button.setFixedWidth(75)
+        self.update_button.setStyleSheet("background-color: #99dee6; color: #ffffff;")
+        self.update_button.clicked.connect(lambda x=False, rid=room_id, desc=description, pers=cantPersons, pr=price: self.show_update_dialog(rid, desc, pers, pr))
 
-                item_layout.addWidget(item_label)
-                item_layout.addWidget(self.update_button)
-                item_layout.addWidget(delete_button)
 
-                room_frame_layout.addLayout(item_layout)
+        delete_button = QPushButton("Borrar")
+        delete_button.setFixedWidth(75)
+        delete_button.setStyleSheet("background-color: #ff0125; color: #ffffff;")
+        delete_button.clicked.connect(lambda x=False, desc=description, pers=cantPersons, pr=price: eself.delete_room(desc, pers, pr))
 
-                self.rooms_layout.addWidget(room_frame)
+        item_layout.addWidget(item_label)
+        item_layout.addWidget(self.update_button)
+        item_layout.addWidget(delete_button)
+        room_frame_layout.addLayout(item_layout)
+
+        return room_frame
 
     def adjust_input_group_width(self):
         self.input_group.setFixedWidth(self.width() * 0.45)
@@ -167,7 +170,6 @@ class Admin(QMainWindow):
         insertRoom(description, capacity, price)
         self.populate_rooms()
 
-
     def delete_room(self, description, cantPersons, price):
         deleteRoom(description, cantPersons, price)
         self.populate_rooms()
@@ -175,7 +177,7 @@ class Admin(QMainWindow):
     def show_update_dialog(self, room_id, description, capacity, price):
         dialog = UpdateRoomDialog(room_id, description, capacity, price, self)
 
-        if dialog.exec_():
+        if dialog.exec():
             updated_description = dialog.get_description()
             updated_capacity = dialog.get_capacity()
             updated_price = dialog.get_price()
